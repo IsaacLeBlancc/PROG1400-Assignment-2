41b7055408bf43122e816a7a444ce1d8efe6d2a3
IsaacleBlanc <isaacleblanc23@gmail.com>
2024-03-07 19:34:16 -0400

    Person constructor

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,10 @@
 public class Person {
     private String name;
     private String address;
+
+
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
 }
