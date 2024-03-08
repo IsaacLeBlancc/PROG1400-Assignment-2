830521cdc9c28c6b9f6321fcb6bd746c540a498e
IsaacleBlanc <isaacleblanc23@gmail.com>
2024-03-07 21:53:49 -0400

    method outlines

--- src/Accounting.java
+++ src/Accounting.java
@@ -1,4 +1,23 @@
 import javax.swing.*;
 
 public class Accounting {
+    public void run() {
+
+    }
+
+    private void enterStudentInformation() {
+
+    }
+
+    private void enterStaffInformation() {
+
+    }
+
+    private int getValidYear() {
+
+    }
+
+    private int getValidYearsOfService() {
+
+    }
 }
