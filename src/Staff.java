b86300c42f35f0cd7e576fedb6524ae535dd852f
IsaacleBlanc <isaacleblanc23@gmail.com>
2024-03-07 20:22:25 -0400

    Staff and student constructors

--- src/Staff.java
+++ src/Staff.java
@@ -1,2 +1,8 @@
 public class Staff extends Person {
+    private int yearsOfService;
+
+    public Staff(String name, String address, int yearOfService) {
+        super(name, address);
+        this.yearsOfService = yearOfService;
+    }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,8 @@
 public class Student extends Person {
+    private int yearsAttended;
+
+    public Student(String name, String address, int yearsAttended) {
+        super(name, address);
+        this.yearsAttended = yearsAttended;
+    }
 }
