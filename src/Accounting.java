8b8f86125f2c8a12ce3c0c79861b4f8e90019de8
IsaacleBlanc <isaacleblanc23@gmail.com>
2024-03-07 22:06:00 -0400

    Student or staff or finish buttons

--- src/Accounting.java
+++ src/Accounting.java
@@ -2,7 +2,8 @@ import javax.swing.*;
 
 public class Accounting {
     public void run() {
-
+        Object[] options = {"Student", "Staff", "Finish"};
+        int userChoice = JOptionPane.showOptionDialog(null, "Select Student or Staff.", "Accounting App", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
     }
 
     private void enterStudentInformation() {
